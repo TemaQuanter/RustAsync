8617bffd17254914bd3058cb3f8387184f6d1d84
Artem Mikheev <artemmikheev@Artems-MacBook-Pro.local>
2023-04-26 19:13:39 +0100

    under_hood

---
+++ chapter2/under_hood/src/main.rs
@@ -0,0 +1,40 @@
+use futures::FutureExt;
+use tokio::time::Sleep;
+use std::future::Future;
+use std::task::Poll;
+use std::time::Duration;
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+
+struct Delay<T> {
+    value: Arc<Mutex<Option<T>>>,
+    delay: Pin<Box<Sleep>>
+} // end struct Delay
+
+
+impl<T> Delay<T> {
+    fn new(value: T, delay: Duration) -> Self {
+        Self {
+            value: Arc::new(Mutex::new(Some(value))),
+            delay: Box::pin(tokio::time::sleep(delay))
+        }
+    } // end new()
+} // end impl Delay
+
+impl<T> Future for Delay<T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        match self.delay.poll_unpin(cx) {
+            Poll::Ready(()) => Poll::Ready((*self.value.lock().unwrap()).take().unwrap()),
+            Poll::Pending => Poll::Pending
+        }
+    } // end poll
+} // end impl Future for Delay
+
+#[tokio::main]
+async fn main() {
+    let ftr: Delay<u32> = Delay::new(18, tokio::time::Duration::from_millis(4500));
+
+    println!("The result is: {}", ftr.await);
+} // end main()
